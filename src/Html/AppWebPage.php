b60b55d01ea22c5bddeb0534d38ebe3967c52341
carr0101 <nicolas.carre@etudiant.univ-reims.fr>
2023-06-12 13:43:55 +0200

    Ajout de l'attribut footer dans la classe WebPage et modification dans la classe AppWebPage

--- src/Html/AppWebPage.php
+++ src/Html/AppWebPage.php
@@ -1,41 +1,48 @@
 <?php
 
-declare(strict_types=1);
-
 namespace Html;
 
 class AppWebPage extends WebPage
 {
-    public function __construct(string $title = "")
-    {
+    /**
+     * Constructeur de la classe AppWebPage.
+     *
+     *
+     * @param string $title Le titre de la page.
+     */
+    public function __construct(string $title = ''){
         parent::__construct($title);
-        $this->appendCssUrl('css/style.css');
+        $this->appendCssUrl("/css/style.css");
+
     }
 
-    public function toHTML(): string
-    {
+    /**
+     * Produire la page Web complète.
+     *
+     * @return string La page HTML complète
+     */
+    public function toHTML():string{
         return <<<HTML
-        <!DOCTYPE html>
-        <HTML lang="fr">
+    <html lang="fr">
         <head>
-            {$this->getHead()}
-            <meta charset='UTF-8'>
-            <title>{$this->getTitle()}</title>
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+         {$this->getHead()}
+         <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+         <title>{$this->getTitle()}</title>
         </head>
         <body>
-        <header class="header">
+        <div class='header'>
             <h1>{$this->getTitle()}</h1>
-        </header>
-        <div class="content">
-        {$this->getBody()}
         </div>
-        <footer class="footer">
-            <a class="footer">{$this->getLastModification()}</a>
-        </footer>
+        <div class='content'>
+            <div class='list'>
+                {$this->getBody()}
+            </div>
+        </div>
+        <div class="footer">{$this->getFooter()}</div>            
         </body>
-        </HTML>
-        HTML;
+    </html>
+    HTML;
     }
 }
