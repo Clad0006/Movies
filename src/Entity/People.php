0e177a3df16e7304c69c84cdc8036ac57be8e255
Cladel Valentin <valentin.cladel@gmail.com>
2023-06-12 14:43:50 +0200

    Ajout de la classe People

---
+++ src/Entity/People.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Entity;
+
+class People
+{
+    private int $id;
+    private int $avatarId;
+    private string $birthday;
+    private string $deathday;
+    private string $name;
+    private string $biography;
+    private string $placeOfBirth;
+
+    /**
+ * @return int
+ */
+    public function getId(): int
+    {
+        return $this->id;
+    }/**
+ * @return int
+ */
+    public function getAvatarId(): int
+    {
+        return $this->avatarId;
+    }/**
+ * @return string
+ */
+    public function getBirthday(): string
+    {
+        return $this->birthday;
+    }/**
+ * @return string
+ */
+    public function getDeathday(): string
+    {
+        return $this->deathday;
+    }/**
+ * @return string
+ */
+    public function getName(): string
+    {
+        return $this->name;
+    }/**
+ * @return string
+ */
+    public function getBiography(): string
+    {
+        return $this->biography;
+    }/**
+ * @return string
+ */
+    public function getPlaceOfBirth(): string
+    {
+        return $this->placeOfBirth;
+    }
+
+}
