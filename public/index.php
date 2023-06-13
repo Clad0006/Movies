9a6c9bb32db3fb5094bd2fff8526408d96bf5f03
carr0101 <nicolas.carre@etudiant.univ-reims.fr>
2023-06-13 15:56:02 +0200

    Modification de la structure HTML + début du design de la page n°2

--- public/index.php
+++ public/index.php
@@ -11,7 +11,7 @@ $webpage->setTitle('Films');
 
 $stmt = MovieCollection::findAll();
 foreach ($stmt as $ligne) {
-    $webpage->appendContent("<div class='film'><a href='movie.php?movieId={$ligne->getId()}'><img src='image.php?imageId={$ligne->getPosterId()}'>"."<div class='titre'>".$webpage->escapeString("{$ligne->getTitle()}")."</a></div></div><br>\n");
+    $webpage->appendContent("<div class='film'><a href='movie.php?movieId={$ligne->getId()}'><img src='image.php?imageId={$ligne->getPosterId()}'>"."<div class='title'>".$webpage->escapeString("{$ligne->getTitle()}")."</a></div></div><br>\n");
 }
 $webpage->appendFooter("<p>Dernière modification : {$webpage->getLastModif()}</p>\n");
 
