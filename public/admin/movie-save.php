b65a31aa2ac65a458790ad36e4a27e596b02d53d
Cladel Valentin <valentin.cladel@gmail.com>
2023-06-13 15:24:12 +0200

    Ajout du programme MovieSave d'enregistrement des données du formulaire

---
+++ public/admin/movie-save.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+use Entity\Movie;
+use Entity\Exception\ParameterException;
+use \Html\Form\MovieForm;
+
+header('Location: index.php');
+
+try {
+    if ($_POST["originalLanguage"]=='' or $_POST["originalTitle"]=='' or $_POST["releaseDate"]=='' or $_POST["runtime"]=='' or $_POST["title"]==''){
+        throw new ParameterException();
+    }
+} catch (ParameterException) {
+    http_response_code(400);
+} catch (Exception) {
+    http_response_code(500);
+}
+
+$form= new MovieForm();
+try {
+    $form->setEntityFromQueryString();
+} catch (ParameterException $e) {
+}
+$form->getMovie()->save();
