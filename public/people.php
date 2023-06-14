400f5ba282e1e1206913bdfa5d2a6ebaaaf73578
carr0101 <nicolas.carre@etudiant.univ-reims.fr>
2023-06-14 11:23:59 +0200

    Ajout du CSS des films sur la page d'acteur

--- public/people.php
+++ public/people.php
@@ -35,19 +35,20 @@ $webpage->appendContent("                        </div>\n");
 $webpage->appendContent("                        <div class='bio'>".$webpage->escapeString("{$people->getBiography()}")."</div>\n");
 $webpage->appendContent("                    </div>\n");
 $webpage->appendContent("                </div>\n");
-$webpage->appendContent("            </div>\n");
 
+
+$webpage->appendContent("<div class='lesFilms'>\n");
 $n = 0;
 foreach ($people_movie as $ligne) {
     if ($ligne->getId()!=null){
-        $webpage->appendContent("<div class='film'><a href='movie.php?movieId={$ligne->getId()}'><img class='afficheFilm' src='image.php?imageId={$ligne->getPosterId()}'><div class='infoFilm'><div class='titreFilm'>".$webpage->escapeString("{$ligne->getTitle()}")."<div class='dateFilm'>".$webpage->escapeString("{$ligne->getReleaseDate()}")."<div class='role'>".$role[$n]->getRole()."</div></div></div></a></div></div>\n");
+        $webpage->appendContent("<div class='film'><a href='movie.php?movieId={$ligne->getId()}'><img class='afficheFilm' src='image.php?imageId={$ligne->getPosterId()}'><div class='infoFilm'><div class='titreDate2'><div class='titreFilm'>".$webpage->escapeString("{$ligne->getTitle()}")."</div><div class='dateFilm'>".$webpage->escapeString("{$ligne->getReleaseDate()}")."</div></div><div class='role'>".$role[$n]->getRole()."</div></a></div></div>\n");
     }
     else{
         $webpage->appendContent("<div class='film'><a href='movie.php?movieId={$ligne->getId()}'><img class='affiche' src='https://c8.alamy.com/compfr/ek1c8x/cinema-film-la-cinematographie-annonce-affiche-ou-flyer-avec-arriere-plan-de-l-espace-vide-ek1c8x.jpg'><div class='infoFilm'><div class='titreFilm'>".$webpage->escapeString("{$ligne->getTitle()}")."<div class='dateFilm'>".$webpage->escapeString("{$ligne->getReleaseDate()}")."<div class='role'>".$role[$n]->getRole()."</div></div></div></a></div></div>\n");
     }
     $n += 1;
 }
-
+$webpage->appendContent("</div>\n");
 
 $webpage->appendFooter("<p>Dernière modification : {$webpage->getLastModif()}</p>\n"."<a href='index.php'> Retourner à l'acceuil</a>");
 
